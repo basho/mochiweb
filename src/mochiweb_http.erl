4b106977cc6f233e7d302d29951fd82f5d234bd8
Steve Vinoski <vinoski@ieee.org>
2011-10-27 01:25:32 -0400

    fix zd864: long HTTP header lines not handled properly
    Due to Erlang bug OTP-9389, the handling of HTTP request header lines
    that exceed the size of the gen_tcp receive buffer is broken when
    using {packet,http} and {packet,httph} modes.
    
    Modify mochiweb to work around this bug by using {packet,line} mode
    instead to collect request header lines and then using
    erlang:decode_packet to parse them. Using {packet,line} allows us to
    avoid reading past the headers into the request body (if present),
    since doing that would require us to carry the body data from the
    header handling functions over into the body handling functions, which
    could be a pretty intrusive and complicated change.
    
    Also add regression unit tests that verify that long HTTP request
    lines and long HTTP header lines are handled correctly.

--- src/mochiweb_http.erl
+++ src/mochiweb_http.erl
@@ -86,18 +86,27 @@ default_body(Req) ->
     default_body(Req, Req:get(method), Req:get(path)).
 
 loop(Socket, Body) ->
-    mochiweb_socket:setopts(Socket, [{packet, http}]),
+    mochiweb_socket:setopts(Socket, [{packet, line}]),
     request(Socket, Body).
 
 request(Socket, Body) ->
+    collect_request_line(Socket, Body, <<>>).
+
+collect_request_line(Socket, Body, Prev) ->
     case mochiweb_socket:recv(Socket, 0, ?REQUEST_RECV_TIMEOUT) of
-        {ok, {http_request, Method, Path, Version}} ->
-            mochiweb_socket:setopts(Socket, [{packet, httph}]),
-            headers(Socket, {Method, Path, Version}, [], Body, 0);
-        {error, {http_error, "\r\n"}} ->
-            request(Socket, Body);
-        {error, {http_error, "\n"}} ->
-            request(Socket, Body);
+        {ok, Bin} ->
+            FullBin = <<Prev/binary, Bin/binary>>,
+            case erlang:decode_packet(http, FullBin, []) of
+                {ok, {http_request, Method, Path, Version}, <<>>} ->
+                    collect_headers(Socket, {Method, Path, Version}, Body,
+                                    <<>>, false);
+                {error, {http_error, "\r\n"}} ->
+                    request(Socket, Body);
+                {error, {http_error, "\n"}} ->
+                    request(Socket, Body);
+                {more, _} ->
+                    collect_request_line(Socket, Body, FullBin)
+            end;
         {error, closed} ->
             mochiweb_socket:close(Socket),
             exit(normal);
@@ -113,26 +122,48 @@ reentry(Body) ->
             ?MODULE:after_response(Body, Req)
     end.
 
-headers(Socket, Request, Headers, _Body, ?MAX_HEADERS) ->
-    %% Too many headers sent, bad request.
-    mochiweb_socket:setopts(Socket, [{packet, raw}]),
-    handle_invalid_request(Socket, Request, Headers);
-headers(Socket, Request, Headers, Body, HeaderCount) ->
-    case mochiweb_socket:recv(Socket, 0, ?HEADERS_RECV_TIMEOUT) of
-        {ok, http_eoh} ->
+collect_headers(Socket, Request, Body, Bin, Trunc) ->
+    case {Trunc, mochiweb_socket:recv(Socket, 0, ?HEADERS_RECV_TIMEOUT)} of
+        {false, {ok, <<"\n">>}} ->
             mochiweb_socket:setopts(Socket, [{packet, raw}]),
-            Req = mochiweb:new_request({Socket, Request,
-                                        lists:reverse(Headers)}),
-            call_body(Body, Req),
-            ?MODULE:after_response(Body, Req);
-        {ok, {http_header, _, Name, _, Value}} ->
-            headers(Socket, Request, [{Name, Value} | Headers], Body,
-                    1 + HeaderCount);
-        {error, closed} ->
+            parse_headers(Socket, Request, Body, <<Bin/binary, "\r\n">>, [], 0);
+        {false, {ok, <<"\r\n">>}} ->
+            mochiweb_socket:setopts(Socket, [{packet, raw}]),
+            parse_headers(Socket, Request, Body, <<Bin/binary, "\r\n">>, [], 0);
+        {_, {ok, More}} ->
+            NewBin = <<Bin/binary, More/binary>>,
+            Size = size(More) - 1,
+            NeedMore = case More of
+                           <<_:Size/binary, "\n">> ->
+                               false;
+                           _ ->
+                               true
+                       end,
+            collect_headers(Socket, Request, Body, NewBin, NeedMore);
+        {_, {error, closed}} ->
             mochiweb_socket:close(Socket),
             exit(normal);
         _Other ->
-            handle_invalid_request(Socket, Request, Headers)
+            handle_invalid_request(Socket, Request, [])
+    end.
+
+parse_headers(Socket, Request, _Body, _Bin, Headers, ?MAX_HEADERS) ->
+    %% Too many headers sent, bad request.
+    handle_invalid_request(Socket, Request, Headers);
+parse_headers(Socket, Request, Body, <<"\r\n">>, Headers, _HeaderCount) ->
+    Req = mochiweb:new_request({Socket, Request, lists:reverse(Headers)}),
+    call_body(Body, Req),
+    ?MODULE:after_response(Body, Req);
+parse_headers(Socket, Request, Body, Bin, Headers, HeaderCount) ->
+    case erlang:decode_packet(httph, Bin, []) of
+        {ok, {http_header, _, Name, _, Value}, More} ->
+            parse_headers(Socket, Request, Body, More,
+                          [{Name, Value} | Headers], 1 + HeaderCount);
+        {more, _} ->
+            handle_invalid_request(Socket, Request, Headers);
+        {error, _Reason} ->
+            mochiweb_socket:close(Socket),
+            exit(normal)
     end.
 
 call_body({M, F}, Req) ->
@@ -271,5 +302,52 @@ range_skip_length_test() ->
     ?assertEqual(invalid_range,
                  range_skip_length({BodySize, none}, BodySize)),
     ok.
+
+long_request_line_test() ->
+    {ok, LS} = gen_tcp:listen(0, [binary, {active, false}]),
+    {ok, Port} = inet:port(LS),
+    spawn_link(fun() ->
+                       {ok, S} = gen_tcp:accept(LS),
+                       try
+                           loop(S, {?MODULE, default_body})
+                       after
+                           gen_tcp:close(S)
+                       end
+               end),
+    {ok, S} = gen_tcp:connect("localhost", Port, [binary, {active, false}]),
+    try
+        Req = "GET /" ++ string:chars($X, 8192) ++ " HTTP/1.1\r\n"
+            ++ "Host: localhost\r\n\r\n",
+        ok = gen_tcp:send(S, Req),
+        inet:setopts(S, [{packet, http}]),
+        {ok, {http_response, {1,1}, 200, "OK"}} = gen_tcp:recv(S, 0),
+        ok
+    after
+        gen_tcp:close(S)
+    end.
+
+long_header_test() ->
+    {ok, LS} = gen_tcp:listen(0, [binary, {active, false}]),
+    {ok, Port} = inet:port(LS),
+    spawn_link(fun() ->
+                       {ok, S} = gen_tcp:accept(LS),
+                       try
+                           loop(S, {?MODULE, default_body})
+                       after
+                           gen_tcp:close(S)
+                       end
+               end),
+    {ok, S} = gen_tcp:connect("localhost", Port, [binary, {active, false}]),
+    try
+        Req = "GET / HTTP/1.1\r\n"
+            ++ "Host: localhost\r\n"
+            ++ "Link: /" ++ string:chars($X, 8192) ++ "\r\n\r\n",
+        ok = gen_tcp:send(S, Req),
+        inet:setopts(S, [{packet, http}]),
+        {ok, {http_response, {1,1}, 200, "OK"}} = gen_tcp:recv(S, 0),
+        ok
+    after
+        gen_tcp:close(S)
+    end.
 
 -endif.
